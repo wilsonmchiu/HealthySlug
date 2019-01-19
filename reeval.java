f9d347b380e958a2b657bf80faa8719b1be98547
azhang36 <azhang36@ucsc.edu>
2019-01-19 00:42:44 -0800

    Update reeval.java

--- reeval.java
+++ reeval.java
@@ -2,6 +2,20 @@ public class reeval
 {
     public static void (String [] args)
     {
-        
+
     }
+    public balance()
+}
+public class info
+{
+    private int fat;
+    private int carb;
+    private int prot;
+    public void set(int a, int b, int c)
+    {
+        fat = a;
+        carb = b;
+        prot = c;
+    }
+    public get()
 }
