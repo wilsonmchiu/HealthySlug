faba775204acd28a0d166e3a629e72f1fd55c58a
Joseph Kyle Wong <josephkylewong@gmail.com>
2019-01-19 10:59:57 -0800

    Scraper

---
+++ Clicker.java
@@ -0,0 +1,188 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.JavascriptExecutor;
+import com.google.common.base.Stopwatch;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.*;
+
+public class Clicker {
+
+	static ArrayList<Food> all_food =new ArrayList<Food>();
+	
+	public static String getMeal(int meal_num)
+	{
+		String meal = "";
+		if (meal_num == 0)
+		{
+			meal = "Breakfast";
+		}
+		else if (meal_num == 1)
+		{
+			meal = "Lunch";
+		}
+		else
+		{
+			meal = "Dinner";
+		}
+		return meal;
+	}
+	
+	public static void getInfo(WebDriver driver, int meal_num, String dining_hall)
+	{
+		String hall = dining_hall;
+		String meal = getMeal(meal_num);
+	 	String name = "";
+    	String portion = "";
+    	String cals = "";
+    	String prot = "";
+    	String carb = "";
+    	String fiber = "";
+    	String sugar = "";
+    	String fat = "";
+    	String fat_s = "";
+    	String transF = "";
+    	String chol = "";
+    	String iron = "";
+    	String sod = "";
+    	String vit_A = "";
+    	String vit_C = "";
+    	String vit_B = "";
+    	
+		for (int row = 3; row < 100; row++)
+        {
+        	try 
+        	{
+        		name = driver.findElement(By.xpath("/html/body/table[2]/tbody/tr[" + row + "]/td[1]/div/a")).getText();
+        		portion = driver.findElement(By.xpath("/html/body/table[2]/tbody/tr[" + row + "]/td[2]/div")).getText();
+        		cals = driver.findElement(By.xpath("/html/body/table[2]/tbody/tr[" + row + "]/td[4]/div[1]")).getText();
+        		prot = driver.findElement(By.xpath("/html/body/table[2]/tbody/tr[" + row + "]/td[5]/div[1]")).getText();
+        		carb = driver.findElement(By.xpath("/html/body/table[2]/tbody/tr[" + row + "]/td[6]/div[1]")).getText();
+        		fiber = driver.findElement(By.xpath("/html/body/table[2]/tbody/tr[" + row + "]/td[7]/div[1]")).getText();
+        		sugar = driver.findElement(By.xpath("/html/body/table[2]/tbody/tr[" + row + "]/td[8]/div[1]")).getText();
+        		fat = driver.findElement(By.xpath("/html/body/table[2]/tbody/tr[" + row + "]/td[9]/div[1]")).getText();
+        		fat_s = driver.findElement(By.xpath("/html/body/table[2]/tbody/tr[" + row + "]/td[10]/div[1]")).getText();
+        		transF = driver.findElement(By.xpath("/html/body/table[2]/tbody/tr[" + row + "]/td[11]/div[1]")).getText();
+        		chol = driver.findElement(By.xpath("/html/body/table[2]/tbody/tr[" + row + "]/td[12]/div[1]")).getText();
+        		iron = driver.findElement(By.xpath("/html/body/table[2]/tbody/tr[" + row + "]/td[13]/div[1]")).getText();
+        		sod = driver.findElement(By.xpath("/html/body/table[2]/tbody/tr[" + row + "]/td[14]/div[1]")).getText();
+        		vit_A = driver.findElement(By.xpath("/html/body/table[2]/tbody/tr[" + row + "]/td[15]/div[1]")).getText();
+        		vit_C = driver.findElement(By.xpath("/html/body/table[2]/tbody/tr[" + row + "]/td[16]/div[1]")).getText();
+        		vit_B = driver.findElement(By.xpath("/html/body/table[2]/tbody/tr[" + row + "]/td[17]/div[1]")).getText();
+        		Food food_item = new Food(hall,meal,name,portion,cals,prot,carb,fiber,sugar,fat,fat_s,transF,chol,iron,sod,vit_A,vit_C,vit_B);
+        		all_food.add(food_item);
+        	}
+        	catch (Exception NoSuchElementException)
+        	{
+        		break;
+        	}
+        }
+	}
+	
+	public static void OpenWeb(String[] dininghall,String hall)
+	{
+		for(int i = 0; i < 3; i++)
+		{
+			AccessNutrition(dininghall[i],i,hall);
+		}
+	}
+	public static void AccessNutrition(String site, int meal_num,String hall)
+	{	
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\josep\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		        
+        driver.get(site);
+        int report_position = 0;
+        for (int i = 3; i < 100; i++)
+        {
+        	String xpath = "/html/body/div[2]/table[1]/tbody/tr[" + i + "]/td[1]/table/tbody/tr/td[1]/table/tbody/tr/td[1]/div/input";
+        	try 
+        	{
+        		driver.findElement(By.xpath(xpath)).click();
+        	}
+        	catch (Exception NoSuchElementException)
+        	{
+        		report_position = i;
+        		break;
+        	}
+        }
+
+        String report = "/html/body/div[2]/table[1]/tbody/tr[" + report_position + "]/td/table/tbody/tr/td[1]/input";
+        driver.findElement(By.xpath(report)).click();	
+        getInfo(driver,meal_num,hall);
+        driver.close();
+	}
+	public static void main(String[] args)
+	{
+		Date now = new Date();
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(now);
+		
+		int month = calendar.get(Calendar.MONTH) + 1;
+		int day = calendar.get(Calendar.DAY_OF_MONTH);
+		int year = calendar.get(Calendar.YEAR);
+				
+		String month_str = "";
+		String day_str = "";
+		String year_str = String.valueOf(year);
+		
+		if (month < 10)
+		{
+			month_str = "0" + month;
+		}
+		else
+		{
+			month_str = String.valueOf(month);
+		}
+		if (day < 10)
+		{
+			day_str = "0" + day;
+		}
+		else
+		{
+			day_str = String.valueOf(day);
+		}
+		
+		String C9_Break = "https://nutrition.sa.ucsc.edu/pickMenu.asp?locationNum=40&locationName=Colleges+Nine+%26+Ten+Dining+Hall&dtdate=" + month_str + "%2F" + day_str + "%2F" + year_str + "&mealName=Breakfast&sName=";
+		String C9_Lunch = "https://nutrition.sa.ucsc.edu/pickMenu.asp?locationNum=40&locationName=Colleges+Nine+%26+Ten+Dining+Hall&dtdate=" + month_str + "%2F" + day_str + "%2F" + year_str + "&mealName=Lunch&sName=";
+		String C9_Dinner = "https://nutrition.sa.ucsc.edu/pickMenu.asp?locationNum=40&locationName=Colleges+Nine+%26+Ten+Dining+Hall&dtdate=" + month_str + "%2F" + day_str + "%2F" + year_str + "&mealName=Dinner&sName=";
+		String[] C9 = {C9_Break,C9_Lunch,C9_Dinner};
+		
+		String Cowell_Break = "https://nutrition.sa.ucsc.edu/pickMenu.asp?locationNum=05&locationName=Cowell+Stevenson+Dining+Hall&dtdate=" + month_str + "%2F" + day_str + "%2F" + year_str + "&mealName=Breakfast&sName=";
+		String Cowell_Lunch = "https://nutrition.sa.ucsc.edu/pickMenu.asp?locationNum=05&locationName=Cowell+Stevenson+Dining+Hall&dtdate=" + month_str + "%2F" + day_str + "%2F" + year_str + "&mealName=Lunch&sName=";
+		String Cowell_Dinner = "https://nutrition.sa.ucsc.edu/pickMenu.asp?locationNum=05&locationName=Cowell+Stevenson+Dining+Hall&dtdate=" + month_str + "%2F" + day_str + "%2F" + year_str + "&mealName=Dinner&sName=";
+		String[] Cowell = {Cowell_Break,Cowell_Lunch,Cowell_Dinner};
+		
+		String Crown_Break = "https://nutrition.sa.ucsc.edu/pickMenu.asp?locationNum=20&locationName=Crown+Merrill+Dining+Hall&dtdate=" + month_str + "%2F" + day_str + "%2F" + year_str + "&mealName=Breakfast&sName=";
+		String Crown_Lunch = "https://nutrition.sa.ucsc.edu/pickMenu.asp?locationNum=20&locationName=Crown+Merrill+Dining+Hall&dtdate=" + month_str + "%2F" + day_str + "%2F" + year_str + "&mealName=Lunch&sName=";
+		String Crown_Dinner = "https://nutrition.sa.ucsc.edu/pickMenu.asp?locationNum=20&locationName=Crown+Merrill+Dining+Hall&dtdate=" + month_str + "%2F" + day_str + "%2F" + year_str + "&mealName=Dinner&sName=";
+		String[] Crown = {Crown_Break,Crown_Lunch ,Crown_Dinner};
+		
+		String Porter_Break = "https://nutrition.sa.ucsc.edu/pickMenu.asp?locationNum=25&locationName=Porter+Kresge+Dining+Hall&dtdate=" + month_str + "%2F" + day_str + "%2F" + year_str + "&mealName=Breakfast&sName=";
+		String Porter_Lunch = "https://nutrition.sa.ucsc.edu/pickMenu.asp?locationNum=25&locationName=Porter+Kresge+Dining+Hall&dtdate=" + month_str + "%2F" + day_str + "%2F" + year_str + "&mealName=Lunch&sName=";
+		String Porter_Dinner = "https://nutrition.sa.ucsc.edu/pickMenu.asp?locationNum=25&locationName=Porter+Kresge+Dining+Hall&dtdate=" + month_str + "%2F" + day_str + "%2F" + year_str + "&mealName=Dinner&sName=";
+		String[] Porter = {Porter_Break,Porter_Lunch ,Porter_Dinner};
+		
+		String Rachel_Break = "https://nutrition.sa.ucsc.edu/pickMenu.asp?locationNum=30&locationName=Rachel+Carson+Oakes+Dining+Hall&dtdate=" + month_str + "%2F" + day_str + "%2F" + year_str + "&mealName=Breakfast&sName=";
+		String Rachel_Lunch = "https://nutrition.sa.ucsc.edu/pickMenu.asp?locationNum=30&locationName=Rachel+Carson+Oakes+Dining+Hall&dtdate=" + month_str + "%2F" + day_str + "%2F" + year_str + "&mealName=Lunch&sName=";
+		String Rachel_Dinner = "https://nutrition.sa.ucsc.edu/pickMenu.asp?locationNum=30&locationName=Rachel+Carson+Oakes+Dining+Hall&dtdate=" + month_str + "%2F" + day_str + "%2F" + year_str + "&mealName=Dinner&sName=";
+		String[] Rachel = {Rachel_Break,Rachel_Lunch ,Rachel_Dinner};
+		
+		OpenWeb(C9,"C9");
+		for (int i = 0; i < all_food.size(); i++)
+		{
+			System.out.println(all_food.get(i).name + " " + all_food.get(i).cals);
+		}
+		//OpenWeb(Cowell,"Cowell");
+		//OpenWeb(Crown,"Crown");
+		//OpenWeb(Porter,"Porter");
+		//OpenWeb(Rachel,"Rachel");
+
+	}
+}
---
+++ Food.java
@@ -0,0 +1,46 @@
+
+public class Food {
+	
+	public String hall;
+	public String meal;
+	public String name;
+	public String portion;
+	public String cals;
+	public String prot;
+	public String carb;
+	public String fiber;
+	public String sugar;
+	public String fat;
+	public String fat_s;
+	public String transF;
+	public String chol;
+	public String iron;
+	public String sod;
+	public String vit_A;
+	public String vit_C;
+	public String vit_B;
+	
+	public Food(String hall,String meal,String name,String portion,String cals,String prot,String carb,String fiber,String sugar,String fat,String fat_s,String transF,String chol,String iron,String sod,String vit_A,String vit_C,String vit_B)
+	{
+		this.hall = hall;
+		this.meal = meal;
+		this.name = name;
+		this.portion = portion;
+		this.cals = cals;
+		this.prot = prot;
+		this.carb = carb;
+		this.fiber = fiber;
+		this.sugar = sugar;
+		this.fat = fat;
+		this.fat_s = fat_s;
+		this.transF = transF;
+		this.chol = chol;
+		this.iron = iron;
+		this.sod = sod;
+		this.vit_A = vit_A;
+		this.vit_C = vit_C;
+		this.vit_B = vit_B;
+	}
+	
+	
+}
