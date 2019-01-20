e7214ae862b450bc3b0b9f1441ea69e3206aa841
wilsonmchiu <wilsonmchiu@gmail.com>
2019-01-19 17:27:40 -0800

    android

---
+++ HealthySlug/app/src/main/java/com/example/wilso/healthyslug/BulkDiet.java
@@ -0,0 +1,13 @@
+package com.example.wilso.healthyslug;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BulkDiet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bulk_diet);
+    }
+}
---
+++ HealthySlug/app/src/main/java/com/example/wilso/healthyslug/DiningHall.java
@@ -0,0 +1,13 @@
+package com.example.wilso.healthyslug;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiningHall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dining_hall);
+    }
+}
